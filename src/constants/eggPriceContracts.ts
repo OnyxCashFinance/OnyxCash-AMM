1ce0b206837b594bb4828871bec72badeb1bd6b7
OnyxCash <onyxcaah7@gmail.com>
2022-02-03 20:10:10 +0530

    dd

---
+++ src/constants/eggPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0x72f7386A1d63485f97C2a9eC0a262ad375e44566',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0xa59a33091577a21ADb26cfb4C8275ABd3095D1d2'
+}
+
+export default priceContracts
